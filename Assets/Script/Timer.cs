b30c183868a4b6907f9e4aa97d3f557fe0e07087
Yu Cheng <aa8180505@hotmail.com>
2023-03-22 15:38:52 +1100

    Week 7

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     public float currentTime = 0;
+	
 	public float timeSpeed = 0.5f;
 	
-	public bool nextTurn = false;
+	private bool _nextTurn = false;
+	
+	
 	
 	[SerializeField] private Bar _timeBar;
 	
+	public bool IsNextTurn() 
+	{
+		return _nextTurn;
+	}
+	
+	public void ResetTimer()
+	{
+		_nextTurn = false;
+		currentTime = 0;
+	}
 	void Update()
 	{
+		if(_nextTurn)
+		{
+			return;
+		}
+		
+		
 		currentTime += timeSpeed * Time.deltaTime;
 		
 		if(currentTime >= 1)
 		{
-			nextTurn = true;
-			currentTime = 0;
+			_nextTurn = true;
 		}
 		_timeBar.SetBar(currentTime,1);
 	}
